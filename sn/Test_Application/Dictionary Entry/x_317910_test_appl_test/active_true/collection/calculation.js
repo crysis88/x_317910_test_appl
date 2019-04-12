005103686722adb6b83ecca75b7f541c92600363
System Administrator <sverma122013@gmail.com>
2019-04-12 14:02:23 +0000

    Changes by 'System Administrator' on 'Test_Application – 1.0.0' -- Build #1

---
+++ sn/Test_Application/Dictionary Entry/x_317910_test_appl_test/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Test_Application
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-04-12 08:50:10
+ * Created By  : admin
+ * Updated On  : 2019-04-12 08:50:10
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=5c7779c1db703300a53cfbef299619ce
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
